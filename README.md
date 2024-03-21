2dfba4782ffc85c93613078ae0badba92b08b0ef
Wilson Chan <wilsonchan54@gmail.com>
2024-03-20 22:50:33 -0400

    chore: Import mui icons / style directly in Navbar component / update README.md

--- README.md
+++ README.md
@@ -6,4 +6,26 @@ npm install --global sanity@latest
 sanity login
 npm create sanity@latest
 
+npm install @mui/icons-material @emotion/styled @emotion/react
 -->
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center" id='readme-top'>
+  <img src="https://www.svgrepo.com/show/39138/portfolio.svg" alt="Logo" width="150" height="150">
+
+  <h3 align="center">[WIP]</h3>
+
+  <p align="center">
+    Web Portfolio powered by Sanity CMS 
+    <br />
+    <br />
+    <a href="https://github.com/WackyChomp/sanity-portfolio"><strong>Explore the docs »</strong></a>
+    <br />
+    <a href="https://github.com/WackyChomp/sanity-portfolio">View Demo</a>
+    ·
+    <a href="https://github.com/WackyChomp/sanity-portfolio/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/WackyChomp/sanity-portfolio/issues">Request Feature</a>
+  </p>
+</div>
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,6 +5,8 @@ import { navLinks } from "@/constants"
 import Link from "next/link"
 import { usePathname, useRouter } from "next/navigation"
 
+import MenuSharpIcon from '@mui/icons-material/Menu';
+
 type Props = {}
 
 const Navbar = (props: Props) => {
@@ -12,29 +14,34 @@ const Navbar = (props: Props) => {
   const router = useRouter();
 
   return (
-    <nav className="bg-blue-500 flex justify-between items-center p-2 backdrop-blur-lg border-4 border-green-600">
+    <nav className="fixed bg-blue-500 w-full flex justify-between items-center p-2 backdrop-blur-lg border-4 border-green-600">
 
       {/* Logo */}
-      <div>
+      <div className="">
         <Image 
           src='https://media1.tenor.com/m/lx2WSGRk8bcAAAAC/pulp-fiction-john-travolta.gif'
           alt='emptiness'
           width={50}
           height={50}
           unoptimized
-          className="bg-red-500 p-1 m-2"
+          className="bg-red-500 p-1 m-2 rounded-lg"
         />
       </div>
 
       {/* Nav Items */}
-      <div className="bg-yellow-400 space-x-5 p-2 border border-none rounded-2xl">
+      <div className="items-center justify-center flex-1 flex bg-yellow-400 space-x-8 mx-8 p-2 border border-none rounded-2xl">
         {navLinks.map((link) =>{
           return(
-            <Link href={link.route} className="bg-blue-400">
+            <Link href={link.route} className="text-2xl bg-blue-400 p-3 hover:font-bold hover:text-red-600">
+              <div className="w-3 h-3 border rounded-full hover:bg-green-400" />
               {link.label}
             </Link>
           )
         })}
+      </div>
+
+      <div className="">
+        <MenuSharpIcon />
       </div>
 
     </nav>
